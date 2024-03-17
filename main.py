727e5a8dc4886ba77a028b704c01af498ff6bf76
Logan <toducphuong7900@gmail.com>
2024-03-17 20:07:05 +0700

    Query parameters

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
-
+from typing import Optional
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return "hellao"
+@app.get("/blog")
+def index(limit = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blog list from db'}
+    else:
+        return {'data': f'{limit} published blog list'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all published blogs'}
+
+# @app.get('/blog/{id}')
+# def show(id: int):
+#     return {'data': id}
+
+
+# @app.get('/blog/{id}/comments')
+# def show(id: int):
+#     return {'data': 'comments'}
+
