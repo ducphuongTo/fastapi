012dd8ef65b17ab214224fbc96b992bd90074822
Logan <toducphuong7900@gmail.com>
2024-03-21 20:51:15 +0700

    Create model and tables

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from . import schemas
-
+from . import schemas, model
+from .database import engine
 
 app = FastAPI()
+
+model.Base.metadata.create_all(bind=engine)
 
 @app.post('/blog')
 def createBlog(request: schemas.Blog):
